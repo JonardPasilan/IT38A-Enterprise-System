07ebed5bd698c5b845be73bc663f932c534dd144
jonard <jonardpasilan106@gmail.com>
2025-05-05 15:29:06 +0800

    added inventory.php

--- Activity1-0424&282025/dashboard.php
+++ Activity1-0424&282025/dashboard.php
@@ -51,8 +51,8 @@
     <div class="dashboard">
         <div class="sidebar">
             <h2>MedTrack Admin</h2>
-            <a href="inventory.html" class="nav-link">📦 Inventory</a>
-            <a href="orders.html" class="nav-link">📋 Orders</a>
+            <a href="inventory.php" class="nav-link">📦 Inventory</a>
+            <a href="orders.php" class="nav-link">📋 Orders</a>
             <a href="billing.html" class="nav-link">💵 Billing</a>
             <a href="reports.html" class="nav-link">📊 Reports</a>
             <a href="reminders.html" class="nav-link">⏰ Reminders</a>
